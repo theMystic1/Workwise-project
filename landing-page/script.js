3527013fdab3b2ab1fa1b905e605259d47a3bf05
theMystic <cluckyugo@gmail.com>
2023-07-18 02:48:40 +0100

    go

--- landing-page/script.js
+++ landing-page/script.js
@@ -1,24 +1,14 @@
 'use strict';
 
-// const sideBar = document.querySelector('.side-bar');
-// const listOutline = document.querySelector('.lstoo');
-// const overLay = document.querySelector('.overlay');
-
-// const openSideBar = function () {
-//   sideBar.classList.remove('hidden');
-//   overLay.classList.remove('hidden');
-// };
-// const closeSideBar = function () {
-//   sideBar.classList.add('hidden');
-//   overLay.classList.add('hidden');
-// };
-// listOutline.addEventListener('click', openSideBar);
-
-// overLay.addEventListener('click', closeSideBar);
-// console.log(sideBar);
 const topBar = document.querySelector('.top-bar');
 const btn = document.querySelector('.btn-mobile-nav');
+const closeSrch = document.querySelector('.rmv');
+const textEl = document.getElementById('text');
 
 btn.addEventListener('click', function () {
   topBar.classList.toggle('nav-open');
+});
+
+closeSrch.addEventListener('click', function () {
+  textEl.value = '';
 });
